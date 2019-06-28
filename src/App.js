1ea8568f5a396f429f731de64dcc412e7214541c
Mario Mishevski <mario.mishevski@webfactory.mk>
2019-06-28 19:17:37 +0200

    Conditionally render view template

--- src/App.js
+++ src/App.js
@@ -2,35 +2,70 @@ import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const app = props => {
-    const [personsState, setPersonsState] = useState({
+const app = () => {
+    const [state, setPersonsState] = useState({
         persons: [
             {name: 'Max', age: 34},
             {name: 'John', age: 23}
         ],
-        otherState: 'some other value'
+        otherState: 'some other value',
+        showPersons: false
     });
 
-    const switchNameHandler = () => {
+    const switchNameHandler = (newName) => {
         setPersonsState({
             persons: [
-                {name: 'Maximilian', age: 34},
+                {name: newName, age: 34},
                 {name: 'John', age: 45}
             ]
         })
     };
 
-    const [otherState, setOtherState] = useState('some other state')
+    const nameChangedHandler = (event) => {
+        setPersonsState({
+            persons: [
+                {name: event.target.value, age: 34},
+                {name: 'John', age: 45}
+            ]
+        })
+    };
 
+    const togglePersonsHandler = () => {
+        const doesShow = state.showPersons;
+        setPersonsState({
+            persons: [
+                {name: 'Max', age: 34},
+                {name: 'John', age: 23}
+            ],
+            otherState: 'some other value',
+            showPersons: !doesShow
+        });
+    };
 
-    console.log(personsState, otherState);
+    const btnStyle = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        marginTop: '1rem',
+        padding: '8px',
+        cursor: 'pointer'
+    };
 
 
     return (
         <div className="App">
-            <button onClick={switchNameHandler}>Click me</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+            <button onClick={togglePersonsHandler} style={btnStyle}>Click me</button>
+            {
+                state.showPersons ?
+                    <div>
+                        <Person name={state.persons[0].name}
+                                age={state.persons[0].age}
+                                changed={nameChangedHandler}
+                                click1={() => switchNameHandler('ANam')}/>
+                        <Person name={state.persons[1].name} age={state.persons[1].age}/>
+                    </div> : null
+            }
+
         </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Person.css'
 
-const person = ( props ) => {
+const person = (props) => {
     return (
-        <div>
-            <p>I',m {props.name}, and I'm {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click1}>I',m {props.name}, and I'm {props.age} years old!</p>
+            {/*two way data binding*/}
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 };
